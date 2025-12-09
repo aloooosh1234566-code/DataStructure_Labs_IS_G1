55acdd4fd1c30bc857ca0b5e9d8b9770337fb512
suada <suadaljuaid1@gmail.com>
2025-12-09 21:43:05 +0300

    lab6- SinglyLinkedList -pt1

---
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,74 @@
+package SinglyLinkedList;
+
+public class SinglyLinkedList<E> {
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void addFirst(E data) {
+        Node<E> newNode = new Node(data, head);
+        head = newNode;
+        if (isEmpty())
+            tail = head;
+        size++;
+    }
+
+ public E getFirst() {
+        return head.getData();
+    }
+
+ public E removeFirst() {
+        if (isEmpty()) return null;
+        E delete = head.getData();
+        head = head.getNext();
+        size--;
+        if (isEmpty())
+            tail = head;
+        return delete;
+    }
+ public void display(){
+        Node<E>temp=head;
+        while (temp!=null){
+            System.out.print(temp.getData()+"---->");
+            temp=temp.getNext();
+        }
+     System.out.println("null");
+
+
+}
+class Node<E>{
+private E data;
+private Node<E> next;
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+}
+
+
+}
